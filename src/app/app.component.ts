485de6ca401a7115106117c6563e169923c2478a
Andrew Batallones <batallonesa@gmail.com>
2022-01-13 07:03:11 -0800

    add ping behavior & finish new server form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { catchError, map, Observable, of, startWith } from 'rxjs';
+import { BehaviorSubject, catchError, map, Observable, of, startWith } from 'rxjs';
 import { DataState } from './enum/data-state.enum';
 import { Status } from './enum/status.enum';
 import { AppState } from './interface/app-state';
@@ -12,9 +12,14 @@ import { ServerService } from './service/server.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  appState$: Observable<AppState<CustomResponse>>;
   readonly DataState = DataState;
   readonly Status = Status;
+  private filterSubject = new BehaviorSubject<string>('');
+
+  // This filterSubject is part of RXJS
+  // TODO: deep dive - RXJS
+  appState$: Observable<AppState<CustomResponse>>;
+  filterStatus$ = this.filterSubject.asObservable();
 
   constructor(private serverService: ServerService) {}
 
